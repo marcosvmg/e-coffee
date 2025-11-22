aad059ef41d6d0bf2e80c94fbe8bbf23a9f4e092
marcosvmg <devmarcosvmg@gmail.com>
2025-11-22 19:00:00 -0300

    debug

--- setup/urls.py
+++ setup/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path('', include('website.urls')),
 ]
 
-if settings.DEBUG:
+if not settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# =====================================================================================
