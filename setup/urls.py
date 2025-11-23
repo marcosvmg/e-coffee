b64ef789adca3f7e739f22e22dd4c1aa375c4f6b
marcosvmg <devmarcosvmg@gmail.com>
2025-11-23 14:07:26 -0300

    conflit

--- setup/urls.py
+++ setup/urls.py
@@ -7,7 +7,3 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('website.urls')),
 ]
-
-if not settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-# =====================================================================================
